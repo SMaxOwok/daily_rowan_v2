94e44574fd324271646b525e6b41204c85eda48d
Max Ono <mono@vistahigherlearning.com>
2018-08-12 17:03:49 -0700

    Clear tmp/storage between tests

--- spec/factories/photo.rb
+++ spec/factories/photo.rb
@@ -2,8 +2,7 @@ FactoryBot.define do
   factory :photo do
     after(:build) do |photo|
       photo.image.attach(io: File.open(Rails.root.join("spec", "data", "assets", "images", "portrait.jpg")),
-                           filename: "portrait.jpg",
-                           content_type: "image/jpeg")
+                           filename: "portrait.jpg")
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,6 +19,12 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
+  config.after(:suite) do
+    Rails.root.join("tmp", "storage").children.each do |child|
+      child.rmtree
+    end
+  end
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:deletion, except: %w(settings))
